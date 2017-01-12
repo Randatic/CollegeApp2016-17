6f691ee3c256a75d0f45e197d1dcd6a481d0a4b7
Randy <randatic@gmail.com>
2017-01-12 13:36:38 -0800

    Registration

--- app/src/main/java/io/github/randatic/collegeapp/Presenter/CreateAccountActivity.java
+++ app/src/main/java/io/github/randatic/collegeapp/Presenter/CreateAccountActivity.java
@@ -7,48 +7,72 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+
 import io.github.randatic.collegeapp.R;
 
-public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener{
+public class CreateAccountActivity extends AppCompatActivity{
 
-    private EditText editTextEmail, editTextEmailConfirm, editTextPassword,
+    private EditText editTextEmail, editTextPassword,
             editTextPasswordConfirm, editTextFirstName, editTextLastName;
 
     private Button buttonSubmit;
-
-    @Override
-    public void onClick(View view) {
-        int id = view.getId();
-        switch (id) {
-            case R.id.create_account_button_submit: submit(); break;
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
-        wireWidgets();
-    }
-
-    private void wireWidgets() {
         editTextEmail = (EditText) findViewById(R.id.create_account_editText_email);
-        editTextEmailConfirm = (EditText) findViewById(R.id.create_account_editText_email_confirm);
         editTextPassword = (EditText) findViewById(R.id.create_account_editText_password);
         editTextPasswordConfirm = (EditText) findViewById(R.id.create_account_editText_password_confirm);
         editTextFirstName = (EditText) findViewById(R.id.create_account_editText_name_first);
         editTextLastName = (EditText) findViewById(R.id.create_account_editText_name_last);
 
         buttonSubmit = (Button) findViewById(R.id.create_account_button_submit);
-        buttonSubmit.setOnClickListener(this);
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO Validate Data
+
+                //Create Backendless User
+                BackendlessUser user = new BackendlessUser();
+                user.setEmail(editTextEmail.getText().toString());
+                user.setPassword(editTextPassword.getText().toString());
+                user.setProperty("first_name", editTextFirstName.getText().toString());
+                user.setProperty("last_name", editTextLastName.getText().toString());
+
+                Backendless.UserService.register(user, createUserRegCallBack());
+            }
+        });
+
+        //Backendless Registration
+        Backendless.initApp(this, "A9859B20-C416-976B-FF6B-05FD0247B700", "3813DDD7-9C53-3E90-FFDE-98850AB61600", "v1" );
+
     }
 
-    private void submit() {
+    private AsyncCallback<BackendlessUser> createUserRegCallBack() {
+
+        return new AsyncCallback<BackendlessUser>() {
+            @Override
+            public void handleResponse(BackendlessUser response) {
+                Toast.makeText(CreateAccountActivity.this, "Registration Success!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void handleFault(BackendlessFault fault) {
+                Toast.makeText(CreateAccountActivity.this, ""+fault.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    private void validate() {
         //TODO edit submit() when we implement the database
         if(
                 (editTextEmail.getText().toString().trim().equals("")||
-                        editTextEmailConfirm.getText().toString().trim().equals("")||
                         editTextFirstName.getText().toString().trim().equals("")||
                         editTextLastName.getText().toString().trim().equals("")||
                         editTextPassword.getText().toString().trim().equals("")||
@@ -56,8 +80,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
 
                 ) {
             Toast.makeText(CreateAccountActivity.this, R.string.fill_all_fields, Toast.LENGTH_SHORT).show();
-        } else if(!(editTextEmail.getText().toString().trim().equals(editTextEmailConfirm.getText().toString().trim())&&
-                editTextPassword.getText().toString().trim().equals(editTextPasswordConfirm.getText().toString().trim()))
+        } else if(!editTextPassword.getText().toString().trim().equals(editTextPasswordConfirm.getText().toString().trim())
                 ) {
             Toast.makeText(CreateAccountActivity.this, R.string.not_matching, Toast.LENGTH_SHORT).show();
         } else {
